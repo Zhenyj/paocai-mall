178c2e9510b64848094acae0f0f50bf5ec92a33d
Zhenyj <1271167154@qq.com>
2022-04-03 17:50:54 +0800

    新增查询商品库存功能

--- paocai-ware/src/main/java/com/zyj/paocai/ware/service/impl/WareSkuServiceImpl.java
+++ paocai-ware/src/main/java/com/zyj/paocai/ware/service/impl/WareSkuServiceImpl.java
@@ -3,14 +3,19 @@ package com.zyj.paocai.ware.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zyj.paocai.common.entity.to.SkuHasStockVo;
+import com.zyj.paocai.common.exception.BizCodeEnum;
 import com.zyj.paocai.common.utils.PageUtils;
 import com.zyj.paocai.common.utils.Query;
 import com.zyj.paocai.ware.dao.WareSkuDao;
 import com.zyj.paocai.ware.entity.WareSkuEntity;
 import com.zyj.paocai.ware.service.WareSkuService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.util.List;
 import java.util.Map;
 
 
@@ -18,14 +23,39 @@ import java.util.Map;
 @Service("wareSkuService")
 public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> implements WareSkuService {
 
+    @Autowired
+    WareSkuDao wareSkuDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> wrapper = new QueryWrapper<>();
+
+        String wareId = (String) params.get("wareId");
+        if(StringUtils.hasText(wareId)){
+            wrapper.eq("ware_id", Long.parseLong(wareId));
+        }
+
+        String skuId = (String) params.get("skuId");
+        if(StringUtils.hasText(skuId)){
+            wrapper.eq("sku_id", Long.parseLong(skuId));
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<SkuHasStockVo> getSkuHasStock(List<Long> skuIds) {
+        List<SkuHasStockVo> skuHasStockVos = wareSkuDao.getSkuStockInfoBySkuIds(skuIds);
+        if (skuIds.size() != skuHasStockVos.size()) {
+            log.error(BizCodeEnum.PRODUCT_WARE_EXCEPTION.getMsg());
+            throw new RuntimeException(BizCodeEnum.PRODUCT_WARE_EXCEPTION.getMsg());
+        }
+        return skuHasStockVos;
     }
 
 }
